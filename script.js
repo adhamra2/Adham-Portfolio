bf8379c5e25ab5c71e3a2d1644a45e8854e43b6f
adhamra2 <139923608+adhamra2@users.noreply.github.com>
2023-09-06 23:18:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+// Filter Able ProjectGallery.
+window.addEventListener('load', ()=>{
+    let allTab = document.querySelectorAll('.tab');
+    let allProject = document.querySelectorAll('.project');
+
+    const removeActive=()=> allTab.forEach(tab=>{
+        tab.classList.remove('tab-active')
+    })
+    allTab.forEach(tab=>{
+        tab.addEventListener('click', (event)=>{
+            removeActive();
+            tab.classList.add('tab-active')
+
+            let fillterName = event.target.getAttribute('data-name');
+
+            allProject.forEach(project=>{
+                let projectName = project.getAttribute('data-name')
+                if((projectName == fillterName) || fillterName == 'All'){
+                    project.style.display = '';
+                }else{
+                    project.style.display = 'none';
+                }
+            })
+        });
+    })
+});
+
+// Nav Scroll //
+
+window.addEventListener('scroll',()=>{
+    let nav = document.querySelector('nav');
+
+    nav.classList.toggle('nav-srcoll', window.scrollY>0);
+});
+
+// Typed JS
+
+let typed = new Typed('.typed-text',{
+    Strings:['Web Developer', 'Apps Developer'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true,
+})
+
+// Menu Bar Js //
+
+let menuBar = document.querySelector('.menu-bar');
+let menuBtn = document.querySelector('.menu-btn');
+
+menuBtn.addEventListener('click',()=>{
+    menuBar.classList.toggle('menu-none')
+})
